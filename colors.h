fc9a3d3a2efa6945839d4c178acb6794f77c17ae
Manthee1 <dinja.martin@gmail.com>
2022-12-26 18:12:45 +0100

    More colors

--- colors.h
+++ colors.h
@@ -1,18 +1,27 @@
 #define BOLD "\033[1m"
-#define YELLOW "\033[1;33m"
+#define UNDERLINE "\033[4m"
+#define BLINK "\033[5m"
+#define REVERSE "\033[7m"
+#define CONCEALED "\033[8m"
+
+// Colors
+#define BLACK "\033[0;30m"
 #define RED "\033[1;31m"
 #define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
 #define BLUE "\033[1;34m"
-#define CYAN "\033[1;36m"
 #define MAGENTA "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define WHITE "\033[1;37m"
 
 // Light
-#define LYELLOW "\033[1;93m"
 #define LRED "\033[1;91m"
 #define LGREEN "\033[1;92m"
+#define LYELLOW "\033[1;93m"
 #define LBLUE "\033[1;94m"
-#define LCYAN "\033[1;96m"
 #define LMAGENTA "\033[1;95m"
+#define LCYAN "\033[1;96m"
+#define LWHITE "\033[1;97m"
 
 // Background
 #define BG_BLACK "\033[40m"
@@ -33,3 +42,18 @@
 #define LBG_MAGENTA "\033[105m"
 #define LBG_CYAN "\033[106m"
 #define LBG_WHITE "\033[107m"
+
+//Transparent
+#define TRANSPARENT "\033[48;5;0m"
+
+//Transparent Background
+#define TBLACK "\033[48;5;16m"
+#define TRED "\033[48;5;124m"
+#define TGREEN "\033[48;5;34m"
+#define TYELLOW "\033[48;5;214m"
+#define TBLUE "\033[48;5;21m"
+#define TMAGENTA "\033[48;5;125m"
+#define TCYAN "\033[48;5;37m"
+#define TWHITE "\033[48;5;231m"
+
+

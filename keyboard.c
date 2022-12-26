704057f341ef505e0268c8f07217ffaaeafd605b
Manthee1 <dinja.martin@gmail.com>
2022-12-26 14:27:56 +0100

    Replaced the keymap with a more dynamic system

--- keyboard.c
+++ keyboard.c
@@ -7,23 +7,6 @@
 
 static struct termios oldt, newt;
 
-struct Keymap {
-	char* keyChar;
-	int key;
-};
-
-struct Keymap keymap[] = {
-	{"\033", 27},	  {"\033[A", 0},	{"\033[B", 1},	  {"\033[C", 2},	{"\033[D", 3},	  {"\033[2~", 4},
-	{"\033[3~", 5},	  {"\033[4~", 6},	{"\033[5~", 7},	  {"\033[6~", 8},	{"\033[7~", 9},	  {"\033[8~", 10},
-	{"\033[9~", 11},  {"\033[10~", 12}, {"\033[11~", 13}, {"\033[12~", 14}, {"\033[13~", 15}, {"\033[14~", 16},
-	{"\033[15~", 17}, {"\033[17~", 18}, {"\033[18~", 19}, {"\033[19~", 20}, {"\033[20~", 21}, {"\033[21~", 22},
-	{"\033[23~", 23}, {"\033[24~", 24}, {"\033[25~", 25}, {"\033[26~", 26}, {"\033[28~", 27}, {"\033[29~", 28},
-	{"\033[31~", 29}, {"\033[32~", 30}, {"\033[33~", 31}, {"\033[34~", 32}, {"\033[35~", 33}, {"\033[36~", 34},
-	{"\033[37~", 35}, {"\033[38~", 36}, {"\033[39~", 37}, {"\033[40~", 38}, {"\033[41~", 39}, {"\033[42~", 40},
-	{"\033[43~", 41}, {"\033[44~", 42}, {"\033[45~", 43}, {"\033[46~", 44}, {"\033[47~", 45}, {"\033[48~", 46},
-	{"\033[49~", 47}, {"\033[50~", 48},
-};
-
 int kInit() {
 	tcgetattr(STDIN_FILENO, &oldt);
 	newt = oldt;
@@ -33,12 +16,23 @@ int kInit() {
 
 int parseKeyPress(char* buffer) {
 	if (buffer[0] != 27) return buffer[0];
+	if (buffer[1] == 0) return 27;
 	printf("\b \b");
-	for (int i = 0; i < sizeof(keymap) / sizeof(struct Keymap); i++) {
-		if (strcmp(buffer, keymap[i].keyChar) != 0) continue;
-		printf("Found key: %d\n", keymap[i].key);
-		return keymap[i].key;
+
+	// If the rest starts with a [ it's a special key
+	if (buffer[1] != '[') return -1;
+	// If it doesn't have a second character just return the value of the first
+	if (buffer[3] == 0) return 200 + buffer[2];
+
+	// If it ends with a ~ Return the number after the [ and before the ~
+	if (buffer[strlen(buffer) - 1] == '~') {
+		buffer[strlen(buffer) - 1] = 0;
+		// Take it's number and return it
+		int num = 0;
+		sscanf(buffer + 2, "%d", &num);
+		return 200 + num;
 	}
+
 	return -1;
 }
 int getKeyPress() {
@@ -51,7 +45,7 @@ int getKeyPress() {
 	if (len < 1) return -1;
 	for (int i = 0; i < len; i++) buffer[i] = getchar();
 
-	// Remove inputed characters
+	// Remove inputted characters
 	for (int i = 0; i < len; i++) printf("\b \b");
 
 	return parseKeyPress(buffer);
